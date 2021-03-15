59170e0d73ecbd83466d4eff5cc0cbba10ab3093
lokhandeyogesh7 <lokhandeyogesh7@gmail.com>
2021-03-15 23:31:58 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { MyApp } from './app.component';
+// import { AngularFireModule } from 'angularfire4';
+// import { AngularFireDatabaseModule } from 'angularfire4/database';
+import { FIREBASE_CONFIG } from './firebase.credentials';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database'
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule { }
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyA61Cv5MvfhlGMux5sQzkNc_QWyrSG9Ezg",
+    authDomain: "ionicshoppinglist-d0c29.firebaseapp.com",
+    projectId: "ionicshoppinglist-d0c29",
+    storageBucket: "ionicshoppinglist-d0c29.appspot.com",
+    messagingSenderId: "830815706121",
+    appId: "1:830815706121:web:50b06090a582b0197cacd8",
+    measurementId: "G-7JC1GRWJJ3"
+}
